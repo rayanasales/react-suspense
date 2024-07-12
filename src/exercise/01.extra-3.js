c79e432b7676f17a0522dba119ad754a258de7fc
RayanaSales <rayanaasales@gmail.com>
2024-07-12 11:16:33 -0300

    move create resource into utils and using pokemon fallback

---
+++ src/exercise/01.extra-3.js
@@ -0,0 +1,37 @@
+// Simple Data-fetching
+// 💯 make more generic createResource
+// http://localhost:3000/isolated/exercise/01.extra-2.js
+
+import * as React from 'react'
+import {fetchPokemon, PokemonDataView, PokemonErrorBoundary, PokemonInfoFallback} from '../pokemon'
+import { createResource } from 'utils'
+
+let pokemonResource = createResource(fetchPokemon('pikachu'))
+
+function PokemonInfo() {
+  const pokemon = pokemonResource.read()
+  return (
+    <div>
+      <div className="pokemon-info__img-wrapper">
+        <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+      <PokemonDataView pokemon={pokemon} />
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="pokemon-info-app">
+      <div className="pokemon-info">
+        <PokemonErrorBoundary>
+          <React.Suspense fallback={<PokemonInfoFallback name='pikachu'/>}>
+            <PokemonInfo />
+          </React.Suspense>
+        </PokemonErrorBoundary>
+      </div>
+    </div>
+  )
+}
+
+export default App
