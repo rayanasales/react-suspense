78a74075d129a104227a9de065e45339474cd81c
Rayana Sales <rayanaasales@gmail.com>
2025-01-16 21:40:49 -0300

    exercise 01 - extra credit 3 - pokemon info callback refactoring

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -2,28 +2,13 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-import {PokemonDataView, fetchPokemon, PokemonErrorBoundary} from '../pokemon'
-
-function createResource(promise) {
-  let status = 'pending'
-  let result = promise.then(
-    resolved => {
-      status = 'resolved'
-      result = resolved
-    },
-    rejected => {
-      status = 'rejected'
-      result = rejected
-    },
-  )
-  return {
-    read() {
-      if (status === 'pending' || status === 'rejected') throw result
-      else if (status === 'resolved') return result
-      throw new Error('This should be impossible')
-    },
-  }
-}
+import {
+  PokemonDataView,
+  fetchPokemon,
+  PokemonInfoFallback,
+  PokemonErrorBoundary,
+} from '../pokemon'
+import {createResource} from 'utils'
 
 const pokemonResource = createResource(fetchPokemon('pikachu'))
 
@@ -44,7 +29,7 @@ function App() {
     <div className="pokemon-info-app">
       <div className="pokemon-info">
         <PokemonErrorBoundary>
-          <React.Suspense fallback={<div>Loading Pokemon...</div>}>
+          <React.Suspense fallback={<PokemonInfoFallback name="Pikachu" />}>
             <PokemonInfo />
           </React.Suspense>
         </PokemonErrorBoundary>
